673d551fb9b23ece3b1368f0ad1a79a7fb6faff3
amazonview <chetijasimran@gmail.com>
2025-07-02 16:41:49 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'avg retail'
 
 
 # The full version, including alpha/beta/rc tags
